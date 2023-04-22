1cc904e8b30814271521b777b452cf4d6787d8c8
Vitalii Bychevyi <bychevoyvitaliy@gmail.com>
2023-04-22 13:35:38 +0300

    invoice.py add get, set

--- invoice.py
+++ invoice.py
@@ -10,3 +10,31 @@ class Invoice():
         self.products = products
         self.exchange_rates = exchange_rates
         self.discount_percent = discount_percent
+    
+    def set_date(self,new_date) -> None:
+        self.date = new_date
+
+    def get_date(self) -> str:
+        return self.date
+    
+    def set_name_company(self, new_name_company) -> None:
+        self.name_company = new_name_company
+    
+    def get_name_company(self) -> str:
+        return self.name_company
+    
+    def set_products(self, product_list: list) -> None:
+        self.products = product_list
+
+    def get_products(self) -> list:
+        return self.products
+    
+    def set_exchange_rates(self, new_exchange_rates: str) -> None:
+        self.exchange_rates = new_exchange_rates
+
+    def get_exchange_rates(self) -> str:
+        return self.exchange_rates
+    
+    def set_discount_percent(self, new_discount_percent) -> None:
+        self.discount_percent = new_discount_percent
+    
